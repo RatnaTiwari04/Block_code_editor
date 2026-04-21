5e9b98ce44b04f588ad0a468ed411cf67760a2af
Ratna Durgesh Tiwari <Ratantiwary04@gmail.com>
2026-04-22 04:05:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const { exec } = require('child_process');
+const { exec } = require('child_process'); 
 const { v4: uuidv4 } = require('uuid');
 
 const authRoutes = require('./routes/auth');
